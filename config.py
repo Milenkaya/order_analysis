61d68f774fb957ddbe17ca6d2049539e07bc1471
Milenkaya <jajjlkhanva@rambler.ru>
2026-07-05 14:36:40 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+data_dir = Path("data")
+reports_dir = Path("reports")
+logs_dir = Path("logs")
+
+report_file = "summary_report.csv"
+log_file = "errors.log"
+
+target_status = "Delivered"
